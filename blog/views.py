c6b62fb14db5664eb43a01215db525155b00f3d0
Rangel Sousa <rangelgs17@gmail.com>
2025-11-03 22:35:38 -0300

    conf. padrão templates no app e templates globais

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,15 @@
-from django.http import HttpResponse
-# from django.shortcuts import render
+from django.shortcuts import render
 
 def blog(request):
     print('blog')
-    return HttpResponse("blog do app 1")
+    return render(
+        request,
+        'blog/index.html'
+    )
 
 def exemplo(request):
     print('exemplo')
-    return HttpResponse("exemplo")
-
+    return render(
+        request,
+        'blog/exemplo.html'
+    )
