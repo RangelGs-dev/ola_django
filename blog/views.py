f3d3627e62c66bd9984b83cd6fd48bd87d71be7b
Rangel Sousa <rangelgs17@gmail.com>
2025-11-08 16:28:08 -0300

    Criando urls dinâmicas no Django URL Dispatcher

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
+from blog.data import posts
 
 def blog(request):
     print('blog')
 
     context={
-        'text': 'Olá blog'
+        # 'text': 'Olá blog',
+        'posts': posts
+    }
+
+    return render(
+        request,
+        'blog/index.html',
+        context
+    )
+
+def post(request, id):
+    print('post', id)
+
+    context={
+        'posts': posts
     }
 
     return render(
